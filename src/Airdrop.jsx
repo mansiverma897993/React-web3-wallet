b137b6f882db7a7ac680b2188d712ed6a44c1354
mansiverma897993 <vmansi756@gmail.com>
2025-10-17 18:09:58 +0000

    Initial commit half work is done

---
+++ src/Airdrop.jsx
@@ -0,0 +1,19 @@
+// import { useWallet } from '@solana/wallet-adapter-react';
+// import { use } from 'react';
+import {useConnection, useWallet } from '@solana/wallet-adapter-react'; 
+
+export function Airdrop() {
+    const wallet = useWallet();
+    const { connection } = useConnection();
+
+    async function sendAirdropToUser() {
+        await connection.requestAirdrop(wallet.publicKey, 10);
+        alert("Airdrop Successful");
+    }
+  return (
+    <div>
+      <input type="text" placeholder="Amount"></input>
+      <button onClick={sendAirdropToUser}>Send Airdrop</button>
+    </div>
+  )
+}
